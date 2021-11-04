f00551a479b7c8dbf819b840c9a6efa97f58e697
Marcus-Ambiel <39175034+Marcus-Ambiel@users.noreply.github.com>
2021-11-03 21:59:28 -0300

    Add files via upload

---
+++ Form/script.js
@@ -0,0 +1,104 @@
+const fields = document.querySelectorAll("[required]")
+
+function ValidateField(field) {
+    // logica para verificar se existem erros
+    function verifyErrors() {
+        let foundError = false;
+
+        for(let error in field.validity) {
+            // se não for customError
+            // então verifica se tem erro
+            if (field.validity[error] && !field.validity.valid ) {
+                foundError = error
+            }
+        }
+        return foundError;
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Please fill in this field."
+            },
+            email: {
+                valueMissing: "Email is required",
+                typeMismatch: "Please, fill in a valid mail"
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+        
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function() {
+
+        const error = verifyErrors()
+
+        if(error) {
+            const message = customMessage(error)
+
+            field.style.borderColor = "red"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = "green"
+            setCustomMessage()
+        }
+    }
+}
+
+
+function customValidation(event) {
+
+    const field = event.target
+    const validation = ValidateField(field)
+
+    validation()
+
+}
+
+for( field of fields ){
+    field.addEventListener("invalid", event => { 
+        // eliminar o bubble
+        event.preventDefault()
+
+        customValidation(event)
+    })
+    field.addEventListener("blur", customValidation)
+}
+
+
+document.querySelector("form")
+.addEventListener("submit", event => {
+    console.log("enviar o formulário")
+
+    // não vai enviar o formulário
+    event.preventDefault()
+})
+
+$(".header").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "+" : "+";
+        });
+    });
+
+});
